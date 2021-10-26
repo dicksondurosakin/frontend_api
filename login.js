5bd2edc97f9d020cb625c5c512112016a50eb2c8
dicksondurosakin <durosakindickson@gmail.com>
2021-10-26 21:03:22 +0100

    frontend for api

---
+++ login.js
@@ -0,0 +1,29 @@
+let form = document.getElementById("login-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let formData = {
+    username: form.username.value,
+    password: form.password.value,
+  };
+
+  fetch("http://127.0.0.1:8000/api/users/token/", {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("DATA: ", data.access);
+      if (data.access) {
+        localStorage.setItem("token", data.access);
+        window.location =
+          "file:///C:/Users/USER/Desktop/Dickson/frontend%20devsearch/project_list.html";
+      } else {
+        alert("Username or Password incorrect");
+      }
+    });
+});
